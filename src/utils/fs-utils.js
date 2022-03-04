833c579386e57390ca4143b1c1b1744a591e6384
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-04 14:30:48 +0100

    Update fs-utils.js

--- src/utils/fs-utils.js
+++ src/utils/fs-utils.js
@@ -1,6 +1,6 @@
 import {join} from "path"
-import fs from "fs-extra"
-import uniqid from uniqid
+// import fs from "fs-extra"
+// import uniqid from uniqid
 
 
 export const DATA_FOLDER_PATH = join(process.cwd, "data")
