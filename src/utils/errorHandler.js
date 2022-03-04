b8cc1f4cdd3ff75eba313b31d1f6eb7e36d22dcd
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-03-04 15:28:20 +0100

    Update errorHandler.js

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,7 +1,8 @@
 
 export const errorHandler = (err, req, res, next) => {
 if(err){
-res.send(err.httpStatusCode || 500).send(err)
-}
+res.status(err.statusCode || 500).send(err)
+} else{
 next()
+}
 }
